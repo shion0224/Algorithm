5a36f650ab2051abf098ebf0eeea01a0e5579c2c
shion0224 <shion8214@gmail.com>
2023-10-12 01:35:27 +0900

    318 B AC
    4０分くらいかかってるのですが、、、

--- Atcoder/ABC318/B.cpp
+++ Atcoder/ABC318/B.cpp
@@ -5,8 +5,28 @@ int main() {
     int N;
     cin>>N;
     int A[N],B[N],C[N],D[N];
-    for (int i = 0; i <= N; i++){
+    int ans[1000][1000] = {0};
+    for (int i = 0; i < N; i++){
         cin >> A[i] >> B[i] >> C[i] >> D[i];
     }
-    
+
+    for (int i = 0; i < N; i++){
+        for (int j = A[i]; j < B[i]; j++){
+            for (int k = C[i]; k < D[i]; k++){
+                ans[j][k] += 1;
+            }
+        }
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < 100; i++){
+        for (int j =0 ; j<100; j++){
+            if(ans[i][j] >= 1){
+                cnt ++;
+                // cout << i << " " << j << endl;
+            } 
+
+        }
+    }
+    cout  << cnt << endl;
 }
